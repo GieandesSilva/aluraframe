122022b167d9928c9bf74c80aa8712552b380ae8
Gieandes Silva <contato@gieandessilva.com>
2017-07-29 15:22:29 -0300

    Classe, métodos implementados, get

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,13 +1,27 @@
 class Negociacao {
     
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
 
-    obtemVolume(){
-        var volume = this.quantidade * this.valor;
+    get volume(){
+        var volume = this._quantidade * this._valor;
         return volume;
     }
+
+    get data(){
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
 }
