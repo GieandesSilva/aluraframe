9790bd9fbfd7e2aa75d82cb712a45c1514fb4f0c
Gieandes Silva <contato@gieandessilva.com>
2017-07-31 23:15:14 -0300

    Implementação de Lista de Negociação, Helpers...

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,26 +6,32 @@ class NegociacaoController{ // Responsável pela ação de capturar o conteúdo 
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
         
         event.preventDefault();
-
-        let data = new Date(
-            ...this._inputData.value // ... é o expred operator que passa os ítens do Array como parametros, cada ítem em sequência, iniciando do 0
-            .split('-') // Remove os ítens e os coloca separadamente em cada posição do Array
-            .map((item, indice) => item - indice % 2) // .map navega pelo array passando o conteúdo, a => é uma Arrow Function, Quando tem apenas uma linha de código, pode ser construída dessa forma.
-        );
-
         // Uma instância de negociação, responsável por receber os valores através do constructor e montar o objeto.
-        let negociacao = new Negociacao(
-            data,
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
+    _limpaFormulario(){
+        this._inputData.value = "";
+        this._inputData.focus();
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
     }
 }
---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,28 @@
+class DateHelper {
+
+    constructor(){
+        throw new Error("DateHelper não pode ser instanciada"); // throw lança um erro, retornando o parâmetro descrito.
+    }
+    
+    static dataParaTexto(data) { // static Permite o acesso direto ao método através da classe, não necessitando ser instânciada
+       
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+        //data.getDate()
+        //+ '/' + (data.getMonth() +1)
+        //+ '/' + data.getFullYear();
+    
+    }
+
+    static textoParaData(texto){
+        
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error("A data precisa estar no formato aaaa-mm-dd");
+        
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); 
+         // Remove os ítens e os coloca separadamente em cada posição do Array
+         // .map navega pelo array passando o conteúdo, a => é uma Arrow Function. 
+         // Quando tem apenas uma linha de código, pode ser construída dessa forma.
+
+    }
+
+}
