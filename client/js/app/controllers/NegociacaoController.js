61eae2829c5fc617fc0c2edafffe03b270af9e8c
Gieandes Silva <contato@gieandessilva.com>
2017-08-02 11:48:21 -0300

    Implementação do Remover lista de Negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,7 @@ class NegociacaoController{ // Responsável pela ação de capturar o conteúdo 
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         let negociacaoDOM = $("#negociacoesView");
         this._negociacoesView = new NegociacoesView(negociacaoDOM);
@@ -24,12 +24,19 @@ class NegociacaoController{ // Responsável pela ação de capturar o conteúdo 
         // Uma instância de negociação, responsável por receber os valores através do constructor e montar o objeto.
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso!!!';
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
+    }
+
+    apaga(){
+
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = "Negociações apagadas com sucesso!!!";
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
